330ce607bfda21f8edeb6f77c2d5c7f85913e58e
Michael Seiranian <michaelseiranian@gmail.com>
2022-10-24 01:02:02 +0100

    Add sign up form

---
+++ microblogs/management/commands/seed.py
@@ -0,0 +1,10 @@
+from django.core.management.base import BaseCommand, CommandError
+from faker import Faker
+
+class Command(BaseCommand):
+    def __init__(self):
+        super().__init__()
+        self.faker = Faker('en_GB')
+
+    def handle(self, *args, **options):
+        pass
